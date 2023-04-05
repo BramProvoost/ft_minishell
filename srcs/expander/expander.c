2075e0d1a5e6ee61b7f34769816cfbd3456b72fd
Bram Provoost <bprovoos@f1r2s16.codam.nl>
2023-04-05 17:58:52 +0200

    update algorithm

--- srcs/expander/expander.c
+++ srcs/expander/expander.c
@@ -6,7 +6,7 @@
 /*   By: bprovoos <bprovoos@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/17 10:03:55 by bprovoos      #+#    #+#                 */
-/*   Updated: 2023/04/05 17:50:13 by bprovoos      ########   odam.nl         */
+/*   Updated: 2023/04/05 17:57:20 by bprovoos      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,34 +49,88 @@ char	*get_value_from_env(char *key, t_env *env)
 	return ("");
 }
 
-char	*expand(char *str, t_env *env)
+char *expand_variable(char *str, t_env *env)
 {
-	int		i;
-	int		j;
-	char	*newstr;
-	bool	do_expand;
-
-	i = 0;
-	newstr = NULL;
-	do_expand = true;
+	char *expanded_str = NULL;
+	int i = 0;
 	while (str[i])
 	{
-		if (str[i] == '\'')	// not working in double quotes!
-			do_expand = !do_expand;
-		if (str[i] == '$' && do_expand)
+		if (str[i] == '$')
 		{
-			j = i + 1;
-			while (str[j] && str[j] != ' ' && str[j] != ',' && str[j] != '.'  && str[j] != '\'' && str[j] != '"' && str[j] != '$' && str[j] != '\0')
+			int j = i + 1;
+			while (str[j] && str[j] != ' ' && str[j] != ',' && str[j] != '.' && str[j] != '\'' && str[j] != '"' && str[j] != '$' && str[j] != '\0')
 				j++;
-			newstr = ft_strjoin(newstr, get_value_from_env(ft_substr(str, i + 1, j - i - 1), env));
+			expanded_str = ft_strjoin(expanded_str, get_value_from_env(ft_substr(str, i + 1, j - i - 1), env));
 			i = j;
 			continue;
 		}
-		newstr = ft_strjoin(newstr, ft_strlendump(&str[i], 1));
+		expanded_str = ft_strjoin(expanded_str, ft_strlendump(&str[i], 1));
 		i++;
 	}
-	free(str);
-	return (newstr);
+	return expanded_str;
+}
+
+char *expand_special_cases(char *str)
+{
+	char *expanded_str = NULL;
+	int i = 0;
+	while (str[i])
+	{
+		if (str[i] == '$')
+		{
+			if (str[i + 1] == '$')
+			{
+				expanded_str = ft_strjoin(expanded_str, "$");
+				i += 2;
+				continue;
+			}
+			else if (str[i + 1] == '?')
+			{
+				char *exit_status_str = ft_itoa(g_exit_status);
+				expanded_str = ft_strjoin(expanded_str, exit_status_str);
+				free(exit_status_str);
+				i += 2;
+				continue;
+			}
+		}
+		expanded_str = ft_strjoin(expanded_str, ft_strlendump(&str[i], 1));
+		i++;
+	}
+	return expanded_str;
+}
+
+bool in_quotes(char *str, int index)
+{
+	int quote_count = 0;
+	for (int i = 0; i < index; i++)
+	{
+		if (str[i] == '\'')
+			quote_count++;
+	}
+	return (quote_count % 2 != 0);
+}
+
+char *expand(char *str, t_env *env)
+{
+	char *newstr = NULL;
+	int i = 0;
+	while (str[i])
+	{
+		bool do_expand = !in_quotes(str, i);
+		if (str[i] == '$' && do_expand)
+		{
+			newstr = ft_strjoin(newstr, expand_special_cases(&str[i]));
+			i += 2;
+		}
+		else
+		{
+			newstr = ft_strjoin(newstr, ft_strlendump(&str[i], 1));
+			i++;
+		}
+	}
+	char *expanded_str = expand_variable(newstr, env);
+	free(newstr);
+	return expanded_str;
 }
 
 void	expander(t_token **tokens, t_env *env)
